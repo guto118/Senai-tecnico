91c4707e7eed0a1fb9d1d9846bdfe7e2456c465c
Gustavo Pedro <106572508+guto118@users.noreply.github.com>
2025-05-19 19:28:52 -0300

    19/05/2025
    Primeira aula de Java

---
+++ Java/JavaProject/src/controller/HelloWord.java
@@ -0,0 +1,16 @@
+package controller;
+
+public class HelloWord {
+	public static void main(String[] args) {
+		
+		// exibição que pula linha
+		System.out.println("Olá Mundo");
+		
+		// exibição que não pula linha
+		System.out.print("Olá Mundo");
+		
+		// exibição que pula linha
+		System.out.println("Olá Mundo");
+		
+	}
+}
